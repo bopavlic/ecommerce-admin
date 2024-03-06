6add7e629c40e647d011182b6f271ff09b798e9d
bpavlic <borna.pavlic@yahoo.com>
2024-03-07 00:13:22 +0100

    fix: prisma path

--- app/api/[storeId]/billboards/route.ts
+++ app/api/[storeId]/billboards/route.ts
@@ -1,5 +1,5 @@
-import { auth } from '@clerk/nextjs';
 import { NextResponse } from 'next/server';
+import { auth } from '@clerk/nextjs';
 
 import prismadb from '@/lib/prismadb';
 
@@ -9,11 +9,13 @@ export async function POST(
 ) {
   try {
     const { userId } = auth();
+
     const body = await req.json();
+
     const { label, imageUrl } = body;
 
     if (!userId) {
-      return new NextResponse('Unauthenticated', { status: 401 });
+      return new NextResponse('Unauthenticated', { status: 403 });
     }
 
     if (!label) {
@@ -21,7 +23,7 @@ export async function POST(
     }
 
     if (!imageUrl) {
-      return new NextResponse('Image Url is required', { status: 400 });
+      return new NextResponse('Image URL is required', { status: 400 });
     }
 
     if (!params.storeId) {
@@ -36,7 +38,7 @@ export async function POST(
     });
 
     if (!storeByUserId) {
-      return new NextResponse('Unauthorized', { status: 403 });
+      return new NextResponse('Unauthorized', { status: 405 });
     }
 
     const billboard = await prismadb.billboard.create({
@@ -47,15 +49,15 @@ export async function POST(
       },
     });
 
-    return NextResponse.json(billboard, { status: 201 });
+    return NextResponse.json(billboard);
   } catch (error) {
-    console.log('[BILLBOARDS_POST_ROUTE_ERROR]:', error);
-    return new NextResponse('Internal Server Error', { status: 500 });
+    console.log('[BILLBOARDS_POST]', error);
+    return new NextResponse('Internal error', { status: 500 });
   }
 }
 
 export async function GET(
-  _: Request,
+  req: Request,
   { params }: { params: { storeId: string } }
 ) {
   try {
@@ -69,9 +71,9 @@ export async function GET(
       },
     });
 
-    return NextResponse.json(billboards, { status: 201 });
+    return NextResponse.json(billboards);
   } catch (error) {
-    console.log('[BILLBOARDS_POST_ROUTE_ERROR]:', error);
-    return new NextResponse('Internal Server Error', { status: 500 });
+    console.log('[BILLBOARDS_GET]', error);
+    return new NextResponse('Internal error', { status: 500 });
   }
 }
